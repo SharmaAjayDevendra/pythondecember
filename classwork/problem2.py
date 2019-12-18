0e3f3406d8f3a70362dde15e62a6960d506b9206
Ajay Sharma <ajay90560@gmail.com>
2019-12-18 14:33:03 +0530

    Create problem2.py

---
+++ classwork/problem2.py
@@ -0,0 +1,21 @@
+f=open("myfile.txt")
+while True:
+	k=open("myfile.txt")
+	studentdata=k.read()
+	k.close()
+	student=f.readline()
+	if student=="\n":
+		continue
+	if not student:
+		break
+	s=student.split(": ")
+	print(s[0])
+	att=input("Attendance (p/a): ")
+	if att=="p":
+		temp=int(s[1])+1
+		tempstr=s[0]+": "+str(temp)+"\n"
+		tempstr=studentdata.replace(student, tempstr)
+		g=open("myfile.txt", "w")
+		g.write(tempstr)
+		g.close()
+f.close()
